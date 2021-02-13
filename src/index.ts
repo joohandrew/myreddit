bdcc9e489bf567759eaf12a335061f327e3d8121
joohandrew <andrew.joohan.oh@gmail.com>
2021-02-13 15:41:48 -0800

    set up mikro orm

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  // Run migrations
+  await orm.getMigrator().up();
+
+  // Run
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main();
