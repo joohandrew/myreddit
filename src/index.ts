cccfa3deb6d32ae196f276d55423417ca04cb20b
joohandrew <andrew.joohan.oh@gmail.com>
2021-02-13 15:53:00 -0800

    set up graphql server

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,30 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import express from "express";
+import { ApolloError, ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
 
   // Run migrations
   await orm.getMigrator().up();
 
-  // Run
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main();
